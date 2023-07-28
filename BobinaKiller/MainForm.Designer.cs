7bf7684b18b4ed35ed752b7e5ae0b4b47e0a5793
Luigi <data53589@datainfo.inf.br>
2023-07-28 08:53:37 -0300

    Add project files.

---
+++ BobinaKiller/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Bobinoloide
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.keysLabel = new System.Windows.Forms.Label();
+            this.keysTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.intervalBox = new System.Windows.Forms.TextBox();
+            this.intervalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(59, 128);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(152, 128);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // keysLabel
+            // 
+            this.keysLabel.AutoSize = true;
+            this.keysLabel.Location = new System.Drawing.Point(131, 20);
+            this.keysLabel.Name = "keysLabel";
+            this.keysLabel.Size = new System.Drawing.Size(30, 13);
+            this.keysLabel.TabIndex = 4;
+            this.keysLabel.Text = "Keys";
+            // 
+            // keysTextBox
+            // 
+            this.keysTextBox.Location = new System.Drawing.Point(59, 39);
+            this.keysTextBox.Name = "keysTextBox";
+            this.keysTextBox.Size = new System.Drawing.Size(168, 20);
+            this.keysTextBox.TabIndex = 5;
+            this.keysTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(119, 168);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(47, 15);
+            this.statusLabel.TabIndex = 6;
+            this.statusLabel.Text = "Status";
+            // 
+            // intervalBox
+            // 
+            this.intervalBox.Location = new System.Drawing.Point(59, 97);
+            this.intervalBox.Name = "intervalBox";
+            this.intervalBox.Size = new System.Drawing.Size(168, 20);
+            this.intervalBox.TabIndex = 7;
+            this.intervalBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // intervalLabel
+            // 
+            this.intervalLabel.AutoSize = true;
+            this.intervalLabel.Location = new System.Drawing.Point(116, 79);
+            this.intervalLabel.Name = "intervalLabel";
+            this.intervalLabel.Size = new System.Drawing.Size(64, 13);
+            this.intervalLabel.TabIndex = 8;
+            this.intervalLabel.Text = "Interval (ms)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 202);
+            this.Controls.Add(this.intervalLabel);
+            this.Controls.Add(this.intervalBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.keysTextBox);
+            this.Controls.Add(this.keysLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bobinoloide";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button startButton;
+		private System.Windows.Forms.Button stopButton;
+		private System.Windows.Forms.Label keysLabel;
+		private System.Windows.Forms.TextBox keysTextBox;
+		private System.Windows.Forms.Label statusLabel;
+		private System.Windows.Forms.TextBox intervalBox;
+		private System.Windows.Forms.Label intervalLabel;
+	}
+}
+
---
+++ BobinaKiller/MainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using Interceptor;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Bobinoloide
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+
+        private bool started = false;
+        private BackgroundWorker backgroundWorker1;
+        private Process targetProcess = Process.GetProcessesByName("Bobinoloide").FirstOrDefault();
+        private string targetWindowTitle = "Bobinoloide";
+        private string keysText = string.Empty;
+        private string intervalTimeText = string.Empty;
+        private int intervalTimeInt = int.MinValue;
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            keysText = keysTextBox.Text.Trim();
+            intervalTimeText = intervalBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(keysText))
+            {
+                MessageBox.Show("Favor informar as teclas a serem pressionadas.");
+                return;
+            }
+
+
+            if (string.IsNullOrEmpty(intervalTimeText))
+            {
+                MessageBox.Show("Favor informar o intervalo em milisegundos");
+                return;
+            }
+            else
+                intervalTimeInt = Convert.ToInt32(intervalTimeText);
+
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+            keysTextBox.Enabled = false;
+            intervalBox.Enabled = false;
+
+            statusLabel.Text = "Started";
+            started = true;
+
+            backgroundWorker1 = new BackgroundWorker();
+            backgroundWorker1.DoWork += BackgroundWorker1_DoWork;
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                Input input = new Input();
+
+                // Be sure to set your keyboard filter to be able to capture key presses and simulate key presses
+                // KeyboardFilterMode.All captures all events; 'Down' only captures presses for non-special keys; 'Up' only captures releases for non-special keys; 'E0' and 'E1' capture presses/releases for special keys
+                input.KeyboardFilterMode = KeyboardFilterMode.All;
+                // You can set a MouseFilterMode as well, but you don't need to set a MouseFilterMode to simulate mouse clicks
+
+                // Finally, load the driver
+                input.Load();
+
+                string[] keysArray = keysText.Split(',');
+
+                while (started == true)
+                {
+                    foreach (string key in keysArray)
+                    {
+                        if (!IsWindowActive(targetProcess, targetWindowTitle))
+                        {
+                            input.SendText(key);
+                            Thread.Sleep(intervalTimeInt);
+                        }
+                        else
+                        Thread.Sleep(intervalTimeInt);
+                    }
+                }
+
+                if (started == false)
+                    input.Unload();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro na execução. Stacktrace: " + ex.StackTrace);
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            keysTextBox.Enabled = true;
+            intervalBox.Enabled = true;
+
+            started = false;
+            statusLabel.Text = "Stopped";
+        }
+
+        private static bool IsWindowActive(Process process, string windowTitle)
+        {
+            IntPtr foregroundWindowHandle = GetForegroundWindow();
+
+            if (foregroundWindowHandle != IntPtr.Zero)
+            {
+                uint foregroundProcessId;
+                GetWindowThreadProcessId(foregroundWindowHandle, out foregroundProcessId);
+
+                if (process.Id == foregroundProcessId)
+                {                    
+                    const int maxWindowTitleLength = 256;
+                    var windowTitleBuilder = new System.Text.StringBuilder(maxWindowTitleLength);
+                    int length = GetWindowText(foregroundWindowHandle, windowTitleBuilder, maxWindowTitleLength);
+
+                    if (length > 0)
+                    {
+                        string activeWindowTitle = windowTitleBuilder.ToString().Substring(0, length);
+                        if (activeWindowTitle == windowTitle)
+                        {                            
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
